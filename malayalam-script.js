be16e5a5f9e011c90fd263b3808ce39dc26ee3dc
Ajay Daniel Trevor <59312969+ajayda24@users.noreply.github.com>
2020-07-17 12:16:06 +0530

    Update malayalam-script.js

--- malayalam-script.js
+++ malayalam-script.js
@@ -326,7 +326,53 @@ function submitAnswers(){
         //  document.getElementById("hiding-textbox-three-four").style.display = "none";
         //  document.getElementById("hiding-textbox-three-five").style.display = "none";
 
-         
+         //--------------------------------------------
+        var yourScore = 0;
+
+        if(check_one_one_checked == "1.(a) Correct Answer "){
+            yourScore = yourScore+1;
+        }
+        if(check_one_two_checked == "1.(b) Correct Answer "){
+            yourScore = yourScore+1;
+        }
+        if(check_one_three_checked == "1.(c) Correct Answer "){
+            yourScore = yourScore+1;
+        }
+        if(check_one_four_checked == "1.(d) Correct Answer "){
+            yourScore = yourScore+1;
+        }
+        
+        if(check_two_one_checked == "2.(a) Correct Answer "){
+            yourScore = yourScore+1;
+        }
+        if(check_two_two_checked == "2.(b) Correct Answer "){
+            yourScore = yourScore+1;
+        }
+        if(check_two_three_checked == "2.(c) Correct Answer "){
+            yourScore = yourScore+1;
+        }
+
+        if(check_three_one_checked == "3.(a) Correct Answer "){
+            yourScore = yourScore+1;
+        }
+        if(check_three_two_checked == "3.(b) Correct Answer "){
+            yourScore = yourScore+1;
+        }
+        if(check_three_three_checked == "3.(c) Correct Answer "){
+            yourScore = yourScore+1;
+        }
+        if(check_three_four_checked == "3.(d) Correct Answer "){
+            yourScore = yourScore+1;
+        }
+        if(check_three_five_checked == "3.(e) Correct Answer "){
+            yourScore = yourScore+1;
+        }
+
+
+
+
+
+        document.getElementById("displayScore").innerHTML = yourScore;
       
 
          document.getElementById("one-sub-one-ans-id").disabled = true;
